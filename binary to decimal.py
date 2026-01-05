9f444bb19ebbbdf57b8c2cb2965ed418ae309bfd
KrishanChopra15 <choprakrishan15@gmail.com>
2026-01-05 17:06:50 +0530

    Delete binary to decimal.py

--- binary to decimal.py
+++
@@ -1,8 +0,0 @@
-binary = input ("Enter a binary number:")
-binarylen = len (binary)
-decimal = 0
-for i in range (binarylen,0,-1):
-    number = int (binary[-i])
-    if number == 1:
-        decimal += (2**(i-1))
-print (decimal)
