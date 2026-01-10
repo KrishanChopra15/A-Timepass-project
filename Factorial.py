c33cb4012ce45ceec8ec6e0d25f5d4677e5ec139
KrishanChopra15 <choprakrishan15@gmail.com>
2026-01-10 21:04:08 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -3,4 +3,5 @@ number = int(input("Enter a number:"))
 l = []
 for num in range(number):
     l.append(num + 1)
-print("Factorial of {0} is: {1}".format(number,(math.prod(l))))
+
+print("Factorial of {0} is: {1}".format(number,math.prod(l)))
