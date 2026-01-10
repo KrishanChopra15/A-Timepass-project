92b7ff480f76783d3085bc596f58483c45e48ad4
KrishanChopra15 <choprakrishan15@gmail.com>
2026-01-10 21:01:46 +0530

    Update binary to decimal.py

--- binary to decimal.py
+++ binary to decimal.py
@@ -1,16 +1,15 @@
-binary = input ("Enter a binary number:")
-binarylen = len (binary)
+binary = input ("Enter a binary number:")              
+binarylen = len (binary)                               
 decimal = 0
 if binary.isdigit() is True:
-    for i in range (binarylen,0,-1):
-        number = int (binary[-i])
-        if number == 1:
-            decimal += (2**(i-1))
-        elif number not in (1,0):
-            print ("Enter a valid binary number")
+    for i in range (binarylen,0,-1):                   
+        if int(binary[-i]) == 1:                       
+            decimal += (2**(i-1))                      
+        elif int(binary[-i]) not in (1,0):
+            print ("\nEnter a valid binary number")
             decimal = ""
             break
 else:
-    print ("Enter a valid binary number")
+    print ("\nEnter a valid binary number")
     decimal = ""
-print (decimal)
+print (f"\n{decimal}")
